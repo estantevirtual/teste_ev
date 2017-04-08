21a28fa9c14add609bb00d24391be1d7fe65b695
Bernardo Amaral <bernardoamaral@id.uff.br>
2017-04-08 20:25:01 -0300

    adjusted categories endpoints

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -28,10 +28,8 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
         format.json { render :show, status: :created, location: @category }
       else
-        format.html { render :new }
         format.json { render json: @category.errors, status: :unprocessable_entity }
       end
     end
@@ -42,10 +40,8 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
         format.json { render :show, status: :ok, location: @category }
       else
-        format.html { render :edit }
         format.json { render json: @category.errors, status: :unprocessable_entity }
       end
     end
