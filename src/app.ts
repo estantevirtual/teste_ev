4fd359ad859aa878791748176545fdd8bf167c0f
Mauricio Luiz de toledo <102395706+mlt3800@users.noreply.github.com>
2023-03-23 15:31:07 +0000

    Basedatabase

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,6 @@ export const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.listen(process.env.PORT || 3003, () => {
+app.listen(process.env.PORT || 3300, () => {
     console.log(`Server is running on port ${process.env.PORT || 3003}`)
 })
