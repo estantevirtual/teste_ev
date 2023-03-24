559945e844ac52b94017eb5583e5ae4ee80482fd
Mauricio Luiz de toledo <102395706+mlt3800@users.noreply.github.com>
2023-03-24 10:59:06 +0000

    Correção

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,6 @@ export const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(process.env.PORT || 3003, () => {
     console.log(`Server is running on port ${process.env.PORT || 3003}`)
 })
