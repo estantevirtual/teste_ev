9b26c8cf31b401462240525997b6f87e6b7e2836
Mauricio Luiz de toledo <102395706+mlt3800@users.noreply.github.com>
2023-03-24 09:56:28 +0000

    Correção

--- src/data/BaseDataBase.ts
+++ src/data/BaseDataBase.ts
@@ -12,7 +12,7 @@ export class BaseDatabase {
          user: process.env.DB_USER,
          password: process.env.DB_PASSWORD,
          database: process.env.DB_SCHEMA,
-         port: 3306,
+         port: 3001,
          multipleStatements: true
       }
    })
