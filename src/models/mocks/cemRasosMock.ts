b6744fcec75c1afb919c4e6fd04b210bdf370630
Mauricio Luiz de toledo <mauricioluiz3800@gmail.com>
2023-04-10 15:31:21 -0300

    Force

--- src/models/mocks/cemRasosMock.ts
+++ src/models/mocks/cemRasosMock.ts
@@ -1,13 +1,24 @@
-import { ModalityRepository } from "../../src/repositories/ModalityRepository"
-import { ModalityDTO } from "../../src/models/ModalityDTO"
+import { ModalityRepository } from "../../repositories/ModalityRepository"
+import { ModalityDTO } from "../ModalityDTO"
 import { atletasMocks } from "./atletasMocks"
 
 
 export class AthleteDatabaseMock implements ModalityRepository {
-    async signup (newAthlete: ModalityDTO): Promise<void> {}
+    async createAthlete (newAthlete: ModalityDTO): Promise<void> {}
 
-    async getUserBy (column: string, value: string): Promise< | undefined> {
-        const result = users.filter(item => item.id === value || item.email === value)
+    async getAthleteBy (column: string, value: string): Promise< | any> {
+        const result = atletasMocks.filter(item => item.id === value || item.atleta === value)
         return result[0]
+    }
+    async getAllAthlete(): Promise<ModalityDTO[]|any> {
+        
+       
+    }
+    async ranking(): Promise<ModalityDTO[]|any> {
+        
+        
+    }
+    async deleteModality(competicao: string): Promise<void> {
+        
     }
 }
--- tests/business/testsBusinnes.ts
+++ tests/business/testsBusinnes.ts
@@ -1,7 +1,51 @@
 import { CemMBusiness } from "../../src/Business/100mRasosBusiness";
-import { DardosBusiness } from "../../src/Business/DardosBusiness";
+import { CustomError } from "../../src/errors/CustomError";
+import { ModalityDTO, inputModalityDTO } from '../../src/models/ModalityDTO';
+import { ModalityRepository } from "../../src/repositories/ModalityRepository";
+import { GenId } from "../../src/services/GenId";
 
+export class genIdMock implements GenId {
+    public generateId = jest.fn(() => {
+        return "id"
+    })
+}
+export class cemMocks implements ModalityRepository {
+    async createAthlete (newUser: ModalityDTO): Promise<void> {}
+async getAthleteBy(column: string, value: string): Promise<any> {
+    
+}
+async getAllAthlete(): Promise<ModalityDTO[]|any> {
+    
+}
+async ranking(): Promise<ModalityDTO[]|any> {
+    
+}
+async deleteModality(competicao: string): Promise<void> {
+    
+}}
 const modalityTestsCem = new CemMBusiness(
     new cemMocks(),
     new genIdMock()
-);
+)
+
+
+describe("Testing the create method", () => {
+    test("Should receive an input with no name and return a custom error", async () => {
+        expect.assertions(3)
+        try {
+            const input = {
+                id:"id",
+                competicao: "",
+                atleta: "teste",
+                value: "",
+             unidade: "s"
+            }
+
+            await modalityTestsCem.createAthlete(input)
+        } catch (error: any) {
+            expect(error).toBeInstanceOf(CustomError)
+            expect(error.statusCode).toBe(422)
+            expect(error.message).toBe("Provide the name.")
+        }
+    })
+})
